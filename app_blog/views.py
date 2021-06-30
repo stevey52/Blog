2b7be424d722aab6c323dd0327bc32cd1f59c2f0
mtawa <stevenmtawa@gmail.com>
2021-07-01 00:37:44 +0300

    adding forms

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 # Create your views here.
@@ -14,3 +15,13 @@ class AboutPageView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = "__all__"
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
